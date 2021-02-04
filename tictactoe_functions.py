d1405f8d0dc6d25d8422381d8045511bdbaeb51f
unknown <d.m.stahl@student.tudelft.nl>
2021-02-04 17:12:57 +0100

    all files corrected, issue solved

--- tictactoe_functions.py
+++ tictactoe_functions.py
@@ -1,77 +1,77 @@
 import random
 
-def buildBoard(playerData,showNumbers=False):
-    if showNumbers:
+def build_board(player_data,show_numbers=False):
+    if show_numbers:
         board = ['1','2','3','4','5','6','7','8','9']
     else:
         board = [' ',' ',' ',' ',' ',' ',' ',' ',' ']
     for i in range(9):
-        if i in playerData[0]:
+        if i in player_data[0]:
             board[i] = 'X'
-        elif i in playerData[1]:
+        elif i in player_data[1]:
             board[i] = 'O'
     for j in range(3):
         print("+---+---+---+")
         print("| "+board[3*j]+" | "+board[3*j+1]+" | "+board[3*j+2]+" |")
     print("+---+---+---+")
 
-def doBestTurn(playerData,whoseTurn):
-    if len(playerData[0]|playerData[1])==0:
+def do_best_turn(player_data,whose_turn):
+    if len(player_data[0]|player_data[1])==0:
         return random.choice(range(9))
-    if whoseTurn==1:
-        currentWinner = -2
+    if whose_turn==1:
+        current_winner = -2
     else:
-        currentWinner = 2
-    bestChoices = set()
-    for turn in ((set(range(9))-(playerData[0]|playerData[1]))):
-        scenario = (playerData[0].copy(),playerData[1].copy())
-        scenario[whoseTurn-1].add(turn)
-        nextWhoseTurn = (not(whoseTurn-1))+1
-        winner = whoWins(scenario,nextWhoseTurn)
+        current_winner = 2
+    best_choices = set()
+    for turn in ((set(range(9))-(player_data[0]|player_data[1]))):
+        scenario = (player_data[0].copy(),player_data[1].copy())
+        scenario[whose_turn-1].add(turn)
+        next_whose_turn = (not(whose_turn-1))+1
+        winner = who_wins(scenario,next_whose_turn)
         if winner==2:
             winner = -1
-        if winner==currentWinner:
-            bestChoices.add(turn)
-        elif ( winner>currentWinner and whoseTurn==1 ) or ( winner<currentWinner and whoseTurn==2 ):
-            bestChoices = set()
-            bestChoices.add(turn)
-            currentWinner = winner
-    return random.choice(tuple(bestChoices))
+        if winner==current_winner:
+            best_choices.add(turn)
+        elif ( winner>current_winner and whose_turn==1 ) or ( winner<current_winner and whose_turn==2 ):
+            best_choices = set()
+            best_choices.add(turn)
+            current_winner = winner
+    return random.choice(tuple(best_choices))
 
-def whoWins(playerData,whoseTurn):
-    if checkVictory(playerData):
-        return checkVictory(playerData)
-    elif len(playerData[0]|playerData[1])==9:
+def who_wins(player_data,whose_turn):
+    if check_victory(player_data):
+        return check_victory(player_data)
+    elif len(player_data[0]|player_data[1])==9:
         return 0
-    nextPlayerData = (playerData[0].copy(),playerData[1].copy())
-    nextPlayerData[whoseTurn-1].add(doBestTurn(nextPlayerData,whoseTurn))
-    nextWhoseTurn = (not(whoseTurn-1))+1
-    return whoWins(nextPlayerData,nextWhoseTurn)
+    next_player_data = (player_data[0].copy(),player_data[1].copy())
+    next_player_data[whose_turn-1].add(do_best_turn(next_player_data,whose_turn))
+    next_whose_turn = (not(whose_turn-1))+1
+    return who_wins(next_player_data,next_whose_turn)
 
-def doHumanTurn(playerData,whoseTurn,npcPlays):
-    if npcPlays:
+def do_human_turn(player_data,whose_turn,npc_plays):
+    if npc_plays:
         string = "Dein Zug: "
     else:
-        if whoseTurn==1:
+        if whose_turn==1:
             string = "Spieler 1, dein Zug: "
         else:
             string = "Spieler 2, dein Zug: "
     while True:
-        buildBoard(playerData,True)
+        build_board(player_data,True)
         turn = input(string)
         if turn in ['1','2','3','4','5','6','7','8','9']:
-            if int(turn)-1 in (set(range(9))-(playerData[0]|playerData[1])):
+            if int(turn)-1 in (set(range(9))-(player_data[0]|player_data[1])):
                 return int(turn)-1
 
-def checkVictory(playerData):
+def check_victory(player_data):
     for i in range(3):
         for j in [0,1]:
-            if 3*i+0 in playerData[j] and 3*i+1 in playerData[j] and 3*i+2 in playerData[j]:
+            if 3*i+0 in player_data[j] and 3*i+1 in player_data[j] and 3*i+2 in player_data[j]:
                 return j+1
-            elif i in playerData[j] and i+3 in playerData[j] and i+6in playerData[j]:
+            elif i in player_data[j] and i+3 in player_data[j] and i+6in player_data[j]:
                 return j+1
-            elif 0 in playerData[j] and 4 in playerData[j] and 8 in playerData[j]:
+            elif 0 in player_data[j] and 4 in player_data[j] and 8 in player_data[j]:
                 return j+1
-            elif 2 in playerData[j] and 4 in playerData[j] and 6 in playerData[j]:
+            elif 2 in player_data[j] and 4 in player_data[j] and 6 in player_data[j]:
                 return j+1
     return 0
