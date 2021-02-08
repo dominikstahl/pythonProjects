22210ef5ce9328f02155891db8f72216f1ef0fff
unknown <d.m.stahl@student.tudelft.nl>
2021-02-08 15:09:28 +0100

    added trace and test cases

--- Numpy/numpy_functions.py
+++ Numpy/numpy_functions.py
@@ -13,3 +13,15 @@ def det(matrix):
     for i in range(1,matrix.shape[0],2):
         determinant -= matrix[0,i]*det(np.concatenate((matrix[1:,0:i],matrix[1:,i+1:]),axis=1))
     return determinant
+
+def tr(matrix):
+    if matrix.size==1:
+        return matrix[0,0]
+    if matrix.ndim!=2:
+        raise Exception("matrix not two-dimensional")
+    if matrix.shape[0]!=matrix.shape[1]:
+        raise Exception("matrix not quadratic")
+    trace = 0
+    for i in range(matrix.shape[0]):
+        trace += matrix[i,i]
+    return trace
--- Numpy/test_numpy_functions.py
+++ Numpy/test_numpy_functions.py
@@ -29,3 +29,31 @@ def test_det_5():
 def test_det_6():
     r = det(np.array([[1,2,34,-53,42],[-1,-2,43,-15,-3],[2,4,1,2,0],[50,100,4,45,-70],[-2,-4,0,5,4]]))
     assert r == 0
+
+def test_tr_1():
+    r = tr(np.array([[-2,2,-3],[-1,1,3],[2,0,-1]]))
+    assert r == -2
+
+def test_tr_2():
+    with pytest.raises(Exception) as excinfo:
+        r = tr(np.array([[1,2],[3,4],[5,6]]))
+    assert "matrix not quadratic" in str(excinfo.value)
+
+def test_tr_3():
+    with pytest.raises(Exception) as excinfo:
+        r = tr(np.array([[1,2,3,4],[3,4,5,6],[5,6,7,8]]))
+    assert "matrix not quadratic" in str(excinfo.value)
+
+def test_tr_4():
+    with pytest.raises(Exception) as excinfo:
+        r = tr(np.array([1,2]))
+    assert "matrix not two-dimensional" in str(excinfo.value)
+
+def test_tr_5():
+    with pytest.raises(Exception) as excinfo:
+        r = tr(np.array([[[1,2],[3,4]],[[5,6],[7,8]]]))
+    assert "matrix not two-dimensional" in str(excinfo.value)
+
+def test_tr_6():
+    r = tr(np.array([[1,2,34,-53,42],[-1,-2,43,-15,-3],[2,4,1,2,0],[50,100,4,45,-70],[-2,-4,0,5,4]]))
+    assert r == 49
